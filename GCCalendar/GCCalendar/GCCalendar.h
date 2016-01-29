0bc9835e7ea3960d8f57cd132bd6402717ed321d
Gray Campbell <12163070+graycampbell@users.noreply.github.com>
2016-01-28 18:46:46 -0600

    Remove whitespace

--- GCCalendar/GCCalendar/GCCalendar.h
+++ GCCalendar/GCCalendar/GCCalendar.h
@@ -15,5 +15,3 @@ FOUNDATION_EXPORT double GCCalendarVersionNumber;
 FOUNDATION_EXPORT const unsigned char GCCalendarVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <GCCalendar/PublicHeader.h>
-
-
