792ea4281503847d6dbcffde9a0593647e873965
Gray Campbell <12163070+graycampbell@users.noreply.github.com>
2016-02-03 15:10:25 -0600

    Add more broken code

--- GCCalendar/LayoutConstraint.swift
+++ GCCalendar/LayoutConstraint.swift
@@ -10,10 +10,17 @@ import UIKit
 
 extension NSLayoutConstraint
 {
-    convenience init(i i1: AnyObject, a a1: NSLayoutAttribute, r: NSLayoutRelation = .Equal, i i2: AnyObject? = nil, var a a2: NSLayoutAttribute? = nil, m: CGFloat = 1, c: CGFloat = 0)
+    convenience init(i i1: AnyObject, a a1: NSLayoutAttribute, r: NSLayoutRelation = .Equal, i i2: AnyObject? = nil, var a a2: NSLayoutAttribute = .NotAnAttribute, m: CGFloat = 1, c: CGFloat = 0)
     {
-        a2 = (a2 == nil) ? a1 : a2
+        if i2 != nil && a2 == .NotAnAttribute
+        {
+            a2 = a1
+        }
+        else if i2 == nil && a2 != .NotAnAttribute
+        {
+            a2 = .NotAnAttribute
+        }
         
-        self.init(item: i1, attribute: a1, relatedBy: r, toItem: i2, attribute: a2!, multiplier: m, constant: c)
+        self.init(item: i1, attribute: a1, relatedBy: r, toItem: i2, attribute: a2, multiplier: m, constant: c)
     }
 }
